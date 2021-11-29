047745567366bea5a57c0107d7c138158237297f
akinari <a.mizo0919@gmail.com>
2021-11-29 23:44:02 +0900

    add:rails g作成ファイルをカスタム

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,12 @@ module Gantobashi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    config.generators do |g|
+      g.skip_routes true
+      g.assets false
+      g.helper false
+      g.test_framework false
+    end
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
