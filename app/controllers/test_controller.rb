0d6e388d2a10522fd8059ca74516d0cc3beb167a
akinari <a.mizo0919@gmail.com>
2021-12-03 23:06:39 +0900

    add:test/rusultの追加

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -1,5 +1,22 @@
 class TestController < ApplicationController
   def result
+    uri = URI.parse("https://japanwest.api.cognitive.microsoft.com/face/v1.0/detect")
+    uri.query = URI.encode_www_form({
+      "returnFaceAttributes" => "emotion"
+    })
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = uri.scheme === "https"
+
+    params = { url: "https://i.gyazo.com/17ddb3ed59a19a0c9b01407224f03f7c.jpg" }
+    headers = { 
+      "Content-Type" => "application/json", 
+      "Ocp-Apim-Subscription-Key" => "7cdc69350b194d509669f3bd3b6f88bb" 
+    }
+    response = http.post(uri, params.to_json, headers)
     
+    # render json: response.body
+
+    hash = JSON.parse(response.body)
+    @result  = hash[0]["faceAttributes"]["emotion"]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root 'home#top'
+  get 'test', to: 'test#result'
 end
