eb2db80aa462d0795c004b827ebf181f16e6980b
akinari <a.mizo0919@gmail.com>
2021-12-27 01:35:05 +0900

    improve:ヘッダーフッターを部分テンプレート化

--- app/javascript/packs/home/top.js
+++ app/javascript/packs/home/top.js
@@ -1,67 +1,76 @@
-// /**
-//  * "window.onload":ページの読み込みが完了したタイミングで処理が実行
-//  * "document.querySelector":document全体に対して任意のHTML要素を検出・取得
-//  */
-// window.onload = () => {
-//   const video  = document.querySelector("#camera");
-//   const canvas = document.querySelector("#picture");
+$('#canvas').hide();
+$('#reload').hide();
+// カメラ設定
+const constraints = {
+  video: {
+    width: 300,
+    height: 300,
+    facingMode: "user"   // フロントカメラを利用する
+  }
+};
 
-//   /** カメラ設定 */
-//   const constraints = {
-//     video: {
-//       width: 300,
-//       height: 400,
-//       facingMode: "user"   // フロントカメラを利用する
-//       // facingMode: { exact: "environment" }  // リアカメラを利用する場合
-//     }
-//   };
+// カメラを<video>と同期
+navigator.mediaDevices.getUserMedia(constraints)
+.then( (stream) => {
+  video.srcObject = stream;
+  video.onloadedmetadata = (e) => {
+    video.play();
+  };
+})
+.catch( (err) => {
+  console.log(err.name + ": " + err.message);
+});
 
-//   /**
-//    * カメラを<video>と同期
-//    */
-//   navigator.mediaDevices.getUserMedia(constraints)
-//   .then( (stream) => {
-//     video.srcObject = stream;
-//     video.onloadedmetadata = (e) => {
-//       video.play();
-//     };
-//   })
-//   .catch( (err) => {
-//     console.log(err.name + ": " + err.message);
-//   });
+const URL = "https://teachablemachine.withgoogle.com/models/3tSiNOSPs/";
 
-//   /**
-//    * シャッターボタン
-//    */
-//   document.querySelector("#shutter").addEventListener("click", () => {
-//     // 演出的な目的で一度映像を止める
-//     video.pause();    // 映像を停止
-//     setTimeout( () => {
-//       video.play();    // 0.5秒後にカメラ再開
-//     }, 500);
+let model, labelContainer, maxPredictions;
 
-//     // (チェック用)canvasに画像を貼り付ける
-//     const ctx = canvas.getContext("2d");
-//     ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+async function init() {
+  document.getElementById('shatter').disabled = true;
+  $('#video').hide();
+  $('#canvas').show();
+  $(this).html( $(this).data('loading-text') );
+  
+  // canvasに静止画を入れる
+  var canvas = document.getElementById("canvas")
+  canvas.getContext("2d").drawImage(video, 0, 0, 300, 300)
 
-//     // 画像データをcheck_controllerに渡す
-//     const base64 = canvas.toDataURL('image/jpeg').replace(/data:.*\/.*;base64,/, '');
-//     // alert(base64);
+  // teachablemachineのモデルURLを読み込む
+  const modelURL = URL + "model.json";
+  const metadataURL = URL + "metadata.json";
 
-//     // var xhr = new XMLHttpRequest();
-//     // xhr.open( 'POST', '/check');
-//     // xhr.setRequestHeader( 'X-CSRF-Token', 'meta[name="csrf-token"]' );
-//     // xhr.send();
+  // モデルのイメージを格納する
+  model = await tmImage.load(modelURL, metadataURL);
+  maxPredictions = model.getTotalClasses();
 
-//     $.ajax({
-//       url: '/check',
-//       type: 'POST',
-//       data: {
-//           image: base64
-//         },
-//       headers: {
-//           'X-CSRF-Token' : $('meta[name="csrf-token"]').attr('content')
-//         },
-//     });
-//   });
-// };
+  // 結果を出す為のlavelcontainerをDOMに要素追加する
+  labelContainer = document.getElementById("label-container");
+  window.requestAnimationFrame(loop);
+}
+
+async function loop() {
+  // 予測は、画像、ビデオ、またはキャンバスのhtml要素を取り込むことができます
+  const prediction = await model.predict(canvas);
+
+  // predictionの数値によって結果を変える
+  if (prediction[0].probability.toFixed(2)> 0.1) {
+    const base64 = canvas.toDataURL('image/jpeg').replace(/data:.*\/.*;base64,/, '');
+    $.ajax({
+      url: '/check',
+      type: 'POST',
+      data: {
+          image: base64
+        },
+      headers: {
+          'X-CSRF-Token' : $('meta[name="csrf-token"]').attr('content')
+        },
+      success: function(data) {
+          $(result).append(data);
+        }
+    })
+  } else {
+    $(labelContainer).html('失敗')
+  }
+  $('#shatter').hide();
+  $('#reload').show();
+}
