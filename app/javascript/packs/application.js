f4cdf92e0644a02412c56bf10084004cd1b93666
akinari <a.mizo0919@gmail.com>
2021-12-29 02:20:38 +0900

    add:ハンバーガーメニュー追加

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,8 @@
 import Rails from "@rails/ujs"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import 'bootstrap';
+import '../stylesheets/application.scss'
 
 require('jquery')
 
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,10 +1,15 @@
 const { environment } = require('@rails/webpacker')
 
+// Vue.js フル版（Compiler入り）
+environment.config.resolve.alias = { 'vue$': 'vue/dist/vue.esm.js' }
+
+// jQueryとBootstapのJSを使えるように
 const webpack = require('webpack')
 environment.plugins.prepend('Provide',
     new webpack.ProvidePlugin({
         $: 'jquery/src/jquery',
-        jQuery: 'jquery/src/jquery'
+        jQuery: 'jquery/src/jquery',
+        Popper: ['popper.js', 'default']
     })
 )
 
