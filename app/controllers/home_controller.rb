126e1079cf7e4b44f0251765455afc8cfbd95227
akinari <a.mizo0919@gmail.com>
2021-12-28 22:35:12 +0900

    add:ページ追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def top; end
+  def about; end
+  def info; end
+  def service; end
+  def privacy; end
   def create; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root 'home#top'
+  get 'about', to: 'home#about'
+  get 'info', to: 'home#info'
+  get 'service', to: 'home#service'
+  get 'privacy', to: 'home#privacy'
   get 'create', to: 'home#create'
   post 'check', to: 'check#result'
 end
