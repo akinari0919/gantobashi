8bb813306f0ce040699d59303ec20cd55f7828b2
akinari <a.mizo0919@gmail.com>
2022-01-10 10:51:44 +0900

    improve:本リリース用デザイン修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,7 +20,7 @@ module ApplicationHelper
         description: :description,
         type: 'website',
         url: request.original_url,
-        image: image_url('site_image.png'),
+        image: image_url('logo-card.jpg'),
         locale: 'ja_JP',
       },
       twitter: {
