cb8aac8f4737e72a104c163674b0bd16722a8d17
akinari <a.mizo0919@gmail.com>
2022-01-23 22:30:08 +0900

    add:name入力項目追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-
   def new
     @user = User.new
   end
@@ -16,6 +15,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
