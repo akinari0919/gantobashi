d691432ed319ddde5f9097450d44011cac9b768f
akinari <a.mizo0919@gmail.com>
2022-02-06 18:28:10 +0900

    improve:非ログインユーザーのリダイレクトを設定 #58

--- app/controllers/mode/battle_controller.rb
+++ app/controllers/mode/battle_controller.rb
@@ -1,9 +1,18 @@
 class Mode::BattleController < ApplicationController
+  before_action :login_user
   def index
-    @users = User.where.not(id: current_user.id)
+    @users = User.where.not(id: current_user.id).shuffle
   end
 
   def show
     @user = User.find(params[:id])
+  end
+
+  private
+
+  def login_user
+    unless logged_in?
+      redirect_to login_path
+    end
   end
 end
