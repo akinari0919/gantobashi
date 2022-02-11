ea150ddce932cc3c644c317517421b47350c8ab5
akinari <a.mizo0919@gmail.com>
2022-02-11 15:46:54 +0900

    add:他ユーザー公開設定画像のみ表示 #58

--- app/controllers/mode/battle_controller.rb
+++ app/controllers/mode/battle_controller.rb
@@ -1,7 +1,7 @@
 class Mode::BattleController < ApplicationController
   before_action :login_user
   def index
-    @users = User.where.not(id: current_user.id).shuffle
+    @users = User.where.not(id: current_user.id)
   end
 
   def show
