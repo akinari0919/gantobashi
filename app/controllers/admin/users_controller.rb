48b0ee0942d756e1decdb92047763441ebb91b10
akinari <a.mizo0919@gmail.com>
2022-03-06 23:54:51 +0900

    add:管理画面デザイン調整

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -5,4 +5,31 @@ class Admin::UsersController < Admin::BaseController
     users = User.all
     @users = users.order(created_at: :desc)
   end
+
+  def edit; end
+
+  def update
+    if @user.update(user_params)
+      redirect_to admin_user_path(@user)
+    else
+      render :edit
+    end
+  end
+
+  def show; end
+
+  def destroy
+    @user.destroy!
+    redirect_to admin_users_path
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :name, :role)
+  end
 end
