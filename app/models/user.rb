76df220f9151ca815de429ac5215df207ee4f16d
akinari <a.mizo0919@gmail.com>
2022-03-14 16:38:10 +0900

    improve:name文字数上限を10に変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 
   validates :email, uniqueness: true, presence: true
-  validates :name, presence: true, length: { maximum: 255 }
+  validates :name, presence: true, length: { maximum: 10 }
 
   enum role: { general: 0, admin: 1 }
 end
